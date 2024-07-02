40f0b227a742cfe7bbab42078ea4adc8e6396dd6
Yukihira <Nandwa Japheth>
2024-07-02 12:34:44 +0300

    additions file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 # installs the package puppet-lint
 
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require => Package['flask'],
+}
+
